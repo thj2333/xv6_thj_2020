9f50a251a8b0d7ae18c7ea5258b0815f321b1807
200111413 <200111413@comp2.hwccluster.labcenter>
2022-12-18 03:10:22 +0000

    lab lazy

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,9 +46,16 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  struct proc *p = myproc();
+  addr = p->sz;
+  if (p->sz + n >= MAXVA || p->sz + n < 0) {
+    return addr;
+  }
+
+  if (n < 0) {
+    uvmdealloc(p->pagetable, addr, addr + n);
+  }
+  p->sz = p->sz + n;
   return addr;
 }
 
